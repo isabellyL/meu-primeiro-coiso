6190e3473b1c80ccadbec57798faae1f8ff7b353
isabellyL <156119760+isabellyL@users.noreply.github.com>
2024-01-25 17:30:38 -0300

    conectou ao banco de dados

---
+++ banco de dados/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import * as firestore from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1PMgotwGwz1-BdlMTBmDqVqw9bVtdk1o",
+  authDomain: "teste-55209.firebaseapp.com",
+  projectId: "teste-55209",
+  storageBucket: "teste-55209.appspot.com",
+  messagingSenderId: "878671780183",
+  appId: "1:878671780183:web:22828936143587e2818612",
+  measurementId: "G-JSDV0RJJ5L"
+};
+
+// Initialize Firebase
+console.log("conectado ao firebase")
+const Firebase = initializeApp(firebaseConfig);
+export const db = firestore.getFirestore(Firebase)
+
+
+
+export{ firestore }
