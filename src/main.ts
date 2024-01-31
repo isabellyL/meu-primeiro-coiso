9e0fdc4d5db8bb68322a61870a7b30625e67e27f
isabellyL <156119760+isabellyL@users.noreply.github.com>
2024-01-31 17:45:26 -0300

    criando rotas

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { db, firestore } from '../banco de dados/firebase';
 
 const app = express()
 
-app.listen(3000,function(){
-    console.log('servidor rodando em http://localhost:3000')
+app.get('/', (req, res) => {
+    res.send("bem vindo ao meu primeiro coiso");
+});
+
+app.post("/usuario", async (req, res) => {
+    const usuario = req.body.nome
+
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, "usuario"),{
+            nome: nome,  
+    })
+    res.send(docRef.id)
+    }catch (e){
+    console.log(e)
+    res.status(500).send(e)
+    }
 })
+
+
+
+app.listen(3000, function () {
+    console.log('servidor rodando em http://localhost:3000')
+});
